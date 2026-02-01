d3f965e83e138edf1ac2ecc559619f3d27818a5d
Vask <67902539+TvattBj0rn@users.noreply.github.com>
2026-02-01 18:40:50 +0100

    remove prints and add command print for test value

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ async def parser() -> None:
     # sys.argv -> 0 is the binary
     if len(sys.argv) == 1:
         return
-    print(sys.argv)
+
     if sys.argv[1].lower() == '--wiki':
         await run_db_wiki_update(*sys.argv[2:])
+    if sys.argv[1].lower() == 'test':
+        print(f'This seems to be working: {sys.argv}')
 
 
 if __name__ == '__main__':
